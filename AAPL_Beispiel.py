334a04993080a45fd8e2bef7f8f1572551be3b10
xylokopa <xylokopa@gmail.com>
2026-07-02 09:43:38 +0200

    Add files via upload

--- AAPL_Beispiel.py
+++ AAPL_Beispiel.py
@@ -1,11 +1,12 @@
-# AAPL_Beispiel.py  50Zeilen Aktiensparen zwischen 2019 und 2021
+# AAPL_Beispiel.py  Übungsbeispiel 50Zeilen Aktiensparen zwischen 2019 und 2021
 import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 import os
-csv_Nam ="02Apple_Offline.csv"  # geht von 2015 bis 2026,boersentaeglich!
+# %matplotlib widget
+csv_Nam ="02Apple_Offline.csv"  # geht von 2015 bis 2026
 if os.path.exists(csv_Nam):
    print(f"lokale csv {csv_Nam} wird geladen...")
    df_csv = pd.read_csv(csv_Nam)
@@ -25,7 +26,7 @@ for date, row in df_monthly.iterrows():
     rate  = round(units,6)
     raten_nr = raten_nr + 1
     print(raten_nr,datum,rate)
-    raten_plan.append((rate))
+    raten_plan.append(rate)
 # depot-wert
 gesamte_anteile = sum(raten_plan)
 letzter_preis = df_monthly["Price"].iloc[-1]
@@ -33,16 +34,17 @@ depot_wert = gesamte_anteile * letzter_preis
 # x-werte entspr. monatlich
 x_zahlen = np.arange(len(df_monthly))
 y = raten_plan
-x_labels = [date.strftime("%d-%m-%y") for date in df_monthly.index]
-plt.figure(figsize=(15, 6))
-plt.plot(x_zahlen, y, label='Invest. 20€/mtl', color='red', linestyle='solid') 
+x_labels = [date.strftime("%m-%y") for date in df_monthly.index]
+plt.figure(figsize=(8, 4))
+plt.plot(x_zahlen, y, label='Invest. 20Eu‚/mtl' 
+                    , color='red', linestyle='solid') 
 # x-Achse enstspr Anzahl der Werte
 plt.xlim(0, len(df_monthly) - 1)
 # Anzeige jeder zweite Monat
 plt.xticks(ticks=x_zahlen[::2], labels=x_labels[::2], rotation=0)
-titel_text = (f"Aktien-Sparen mit AAPL (2019-2021)\n"
-              f"Gesamt-Anteile: {gesamte_anteile:.4f} Stück | "
-              f"Depot-Wert: {depot_wert:.2f} € (Kurs: {letzter_preis:.2f} €)")
+titel_text = (f"AAPL-Aktie(2019-2021)\n"
+              f"Anteile: {gesamte_anteile:.4f} Stueck | "
+              f"Depot  : {depot_wert:.2f} Eu (Kurs: {letzter_preis:.2f} Eu)")
 plt.title(titel_text, fontsize=12, fontweight='bold')
 plt.grid(True, linestyle='--', alpha=0.5)
 plt.legend()
