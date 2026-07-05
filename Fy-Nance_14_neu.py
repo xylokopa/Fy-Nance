c86e8f9e39af9daf62eee5b62716f83daf1d136a
xylokopa <xylokopa@gmail.com>
2026-07-06 00:39:50 +0200

    Add files via upload

--- Fy-Nance_14_neu.py
+++ Fy-Nance_14_neu.py
@@ -1,39 +1,3 @@
-###########################################################################
-#                                                                         #
-#  Before running the scripts, please read the informations given to you  #          
-#                                                                         #
-#    throught simply opening:  https://github.com/xylokopa/Fy-Nance       #
-#                                                                         #
-###########################################################################
-#                                                                         #
-#            If you want to reach the README.md directly, enter :         #
-#                                                                         #
-#             Github.com/xylokopa/Fy-Nance/blob/main/README.md            #
-#                                                                         #                                                                                    
-#        or just walk in, if you are a GitHubber, and look around         #
-#                                                                         #
-#             https://github.com/xylokopa       just because              #                                                                                    
-#                                                                         #                                                                                    
-#        without the subsequent indications, it will be impossible        #                                                                                    
-#                                                                         #                                                                                    
-#                       to run the software                               #                                                                                    
-###########################################################################
-#                                                                         #                                                                                    
-#          Please install GIT, if you want full functionality             #                                                                                    
-#                                                                         #                                                                                    
-#   After that , create a local playground in your Jupyter-Folder ,       #                                                                                    
-#                                                                         #                                                                                    
-#   for example c:\myJupyterFolder\GIT-Download  (if you are Windows...)  #                                                                                    
-#                                                                         #                                                                                    
-#   open a cmd (with admin rights) , walk into the GIT-Download folder    #                                                                                    
-#                                                                         #                                                                                    
-#   and hit    git clone https://Github.com/xylokopa/Fy-Nance             #                                                                                    
-#                                                                         #                                                                                    
-###########################################################################
-#                                                                         #                                                                                    
-#           If you don't have GIT , THE README WILL GUIDE YOU             #                                                                                    
-#                                                                         #                                                                                    
-###########################################################################
 print(r"###########################################################################")
 print(r"#                                                                         #")
 print(r"#  Before running the scripts, please read the informations given to you  #")          
