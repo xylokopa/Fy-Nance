0cf360258e0f83dd251ac332096cd9490593933e
xylokopa <xylokopa@gmail.com>
2026-07-13 12:19:07 +0200

    Add files via upload

--- PY/BOszi_Projekt_prerequisites.py
+++ PY/BOszi_Projekt_prerequisites.py
@@ -1,7 +1,7 @@
 # prerequisites.py - REINER PREREQUISITES-TEST
 # Version 03 R.Wu_GastH_Nr178854
 # für alle nötigen importe
-print("⏳ Prüfe System-Voraussetzungen... Bitte warten...\n")
+print("Prüfe System-Voraussetzungen... Bitte warten...\n")
 try:
     import math
     import numpy as np
@@ -20,19 +20,19 @@ try:
     # Test, ob deutsche Formatierung auf dem PC 
     locale.setlocale(locale.LC_TIME, "de_DE")
     print("==========================================================")
-    print("✅ ALLES OK ! Die nötigen Pakete sind installiert.")
+    print("ALLES OK ! Die nötigen Pakete sind installiert.")
     print("Das System ist bereit für den Börsen-Oszillographen.")
     print("==========================================================")
 except ModuleNotFoundError as e:
     # Fehlerfalle, sobald IDLE läuft
     print("==========================================================")
-    print(f"❌ FEHLER: Dir fehlt ein wichtiges Paket!")
+    print(f"FEHLER: Dir fehlt ein wichtiges Paket!")
     print(f"   {e}")
-    print("\n👉 REPARATUR-BEFEHL FÜR DEINE CMD:")
+    print("\n REPARATUR-BEFEHL FÜR DEINE CMD:")
     print("   pip install numpy pandas matplotlib scipy statsmodels yfinance")
     print("==========================================================")
 except locale.Error:
-    print("⚠️ HINWEIS : deutsche Locale ('de_DE') nicht aktiv.")
+    print("HINWEIS : deutsche Locale ('de_DE') nicht aktiv.")
     print("   Skript läuft,aber Datumsformate können abweichen.")
 input("\n[ENTER zum Schließen des Prerequisites-Tests]")
 
